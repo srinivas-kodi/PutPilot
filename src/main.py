9912810ec25fa9e4e52bd019ab21a0986cbab335
Srini <sriniwas_k@yahoo.com>
2025-04-01 07:54:53 -0400

    Add logs

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import sys
 from datetime import datetime
+from time import sleep
 
 import yfinance as yf
 import pandas as pd
@@ -10,19 +11,19 @@ from src.ta_util import TAUtil
 def get_ohlc_data(ticker, end_date, period):
     csv_filename = f"{ticker}_ohlc_data.csv"
 
-    # data = yf.download(ticker, period=period)
-    # data.columns = ['_'.join(col).strip() for col in data.columns.values]
-    # data.rename(columns={
-    #     'Close_AAPL': 'close',
-    #     'High_AAPL': 'high',
-    #     'Low_AAPL': 'low',
-    #     'Open_AAPL': 'open',
-    #     'Volume_AAPL': 'volume'
-    # }, inplace=True)
-    # data.reset_index(inplace=True)
-    # data.rename(columns={'Date': 'date'}, inplace=True)
-    # data.to_csv(csv_filename)
-    data = pd.read_csv(csv_filename)
+    data = yf.download(ticker, period=period)
+    data.columns = ['_'.join(col).strip() for col in data.columns.values]
+    data.rename(columns={
+        f'Close_{ticker}': 'close',
+        f'High_{ticker}': 'high',
+        f'Low_{ticker}': 'low',
+        f'Open_{ticker}': 'open',
+        f'Volume_{ticker}': 'volume'
+    }, inplace=True)
+    data.reset_index(inplace=True)
+    data.rename(columns={'Date': 'date'}, inplace=True)
+    data.to_csv(csv_filename)
+    # data = pd.read_csv(csv_filename)
     data.set_index('date', inplace=True)
     return data
 
@@ -38,20 +39,21 @@ def check_bullish_signals(df):
 if __name__ == "__main__":
     today_date = datetime.today().strftime('%Y-%m-%d')
 
-    stocks = ['AAPL']
+    stocks = ['AAPL', 'NVDA', 'MSFT', 'GOOGL', 'AMZN']
     for stock in stocks:
         ohlc = get_ohlc_data(stock, today_date, '1y')
         ohlc['rsi'] = TAUtil.rsi(ohlc, 14)
         ohlc['macd'], ohlc['macd_signal'] = TAUtil.macd(ohlc,12, 26, 9)
 
         ohlc = check_bullish_signals(ohlc)
+
         is_rsi_bullish = ohlc['rsi_bullish'].iloc[-1]
         is_macd_bullish = ohlc['macd_bullish'].iloc[-1]
-
-        print(f"RSI Bullish: {is_rsi_bullish}, MACD Bullish: {is_macd_bullish}")
+        print(f"{stock} RSI Bullish: {is_rsi_bullish}, MACD Bullish: {is_macd_bullish}")
 
         if is_rsi_bullish or is_macd_bullish:
             print(f"Buy signal for {stock} on {today_date}")
-
+            # Sell Put Option
 
         print(ohlc.tail().to_string())
+        sleep(5)
